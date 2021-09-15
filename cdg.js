9c0e007f2e0397b0472f437748614ef2efbfc3ba
AnnefranceFBC <71523877+AnnefranceFBC@users.noreply.github.com>
2021-09-15 19:32:13 -0300

    introduzindo js

---
+++ cdg.js
@@ -0,0 +1,9 @@
+function Converter(){
+    var valorElemento = document.getElementById("valor");
+    var valor = valorElemento.value;
+    var valorEmDolarNumerico = parseFloat(valor);
+
+   console.log(valorEmDolarNumerico); 
+}
+
+console.log("passei por aqui");
